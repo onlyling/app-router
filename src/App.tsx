65317e81b9cf5f468b32129c4c2908a7d6ac983e
LING_ZI_QING <onlylingstudio@gmail.com>
2022-06-27 16:57:29 +0800

    feat: first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useRef, useEffect } from 'react'
+
+import { init, animate } from './lib/render'
+
+import './App.css'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const ContainerRef = useRef<HTMLDivElement>(null)
+
+  useEffect(() => {
+    init(ContainerRef.current as HTMLDivElement)
+    animate()
+  }, [])
+
+  return <div ref={ContainerRef} />
 }
 
-export default App;
+export default App
